5b3a1973067efadbd47231a729236cbcc38acdf5
hp <koushiksharma9939@gmail.com>
2024-08-08 22:07:44 +0530

    first commit

---
+++ src/main/java/com/koushik/controller/ProductController.java
@@ -0,0 +1,39 @@
+package com.koushik.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.koushik.binding.Product;
+
+import jakarta.validation.Valid;
+
+@Controller
+public class ProductController {
+	
+	@GetMapping("/")
+	public String loadForm(Model model)
+	{
+		model.addAttribute("product", new Product());
+		return "index";
+	}
+	
+	@PostMapping("/product")
+	public String handleSubmitBtn(@Valid Product p,BindingResult result,Model model)
+	{
+		System.out.println(p);
+		if(result.hasErrors()) {
+			return "index";
+		}
+		else
+		{
+			model.addAttribute("msg", "Product Saved");
+		}
+		
+		return "index";
+	}
+
+}
